92320bdef74605089a78efc551028f9263b98380
Esteban Santana Santana <MentalPower@GMail.com>
2017-09-26 01:46:41 -0700

    Downgrated to the LTS version of the 6.x branch to match the build version in CodeBuild

--- scripts/install-nodejs.sh
+++ scripts/install-nodejs.sh
@@ -8,5 +8,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 8 # Current latest of the 8.x branch
+nvm install --lts 6 # Current latest of the 6.x lts branch
 npm install -g forever yarn
