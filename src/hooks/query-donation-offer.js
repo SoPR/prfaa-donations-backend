537faeb734d2bdaf2e54f8465c003b2d7dc2cc0d
Neil A. Beardsley <nabeards@me.com>
2017-09-28 00:23:55 -0600

    Adds $search query support.

--- src/hooks/query-donation-offer.js
+++ src/hooks/query-donation-offer.js
@@ -1,9 +1,25 @@
-const levelQuery = require('level-query');
+const donationOffer = require('../models/donation-offer.model')();
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     return function queryDonationOffer(hook) {
         const query = hook.params.query;
-        Object.keys(query).forEach(key => query[key] = {$like: query[key]});
+
+        if (query.$search) {
+            const search = query.$search;
+            query.$search = [];
+            Object.keys(donationOffer.properties).forEach(doKey => {
+                query.$search.push({[doKey]: {$like: search}});
+            });
+            Object.keys(query).forEach(qKey => {
+                if (qKey !== '$search') {
+                    delete query[qKey];
+                }
+            });
+        }
+        else {
+            Object.keys(query).forEach(key => query[key] = {$like: query[key]});
+        }
+
         return Promise.resolve(hook);
     };
 };
