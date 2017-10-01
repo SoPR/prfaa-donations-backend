8d2a2b629a25fc9c0fd6a6898d69a45deb1412e9
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-10-01 00:17:54 -0400

    Implement email template styles; extract into partials

--- src/hooks/send-email-confirmation.js
+++ src/hooks/send-email-confirmation.js
@@ -1,49 +1,50 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
-const fs    = require('fs');
-const hogan = require('hogan.js');
+const fs        = require('fs');
+const hogan     = require('hogan.js');
+const templates = {
+    html: fs.readFileSync('src/templates/email-confirmation-html.hogan', 'utf8'),
+    text: fs.readFileSync('src/templates/email-confirmation-text.hogan', 'utf8'),
+    partials: {
+        'body-open': fs.readFileSync('src/templates/partials/email-body-open.hogan', 'utf8'),
+        'body-close': fs.readFileSync('src/templates/partials/email-body-close.hogan', 'utf8'),
+        'footer': fs.readFileSync('src/templates/partials/email-footer.hogan', 'utf8'),
+        'header': fs.readFileSync('src/templates/partials/email-header.hogan', 'utf8'),
+        'social-contact': fs.readFileSync('src/templates/partials/email-social-contact.hogan', 'utf8'),
+        'styles': fs.readFileSync('src/templates/partials/email-styles.hogan', 'utf8'),
+    }
+}
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     return function sendEmailConfirmation(hook) {
         return new Promise((rsv, rej) => {
-            fs.readFile('src/templates/email-confirmation-html.hogan', (err, htmlTemplate) => {
-                if (err) {
-                    return rej(err);
+            const app              = hook.app;
+            const transporter      = app.get('nodemailerService');
+            const fromEmail        = app.get('fromEmail');
+            const testToEmail      = app.get('testToEmail');
+            const confirmationLink = app.get('linksUrl') + '/confirm?id=';
+            const htmlEmail        = hogan.compile(templates.html);
+            const textEmail        = hogan.compile(templates.text);
+            const context          = Object.assign({confirmationLink: confirmationLink}, hook.data);
+
+            transporter.sendMail({
+                from:    fromEmail,
+                to:      testToEmail || context.email,
+                subject: 'Please confirm your donation offer to aid Puerto Rico',
+                text:    textEmail.render(context, templates.partials),
+                html:    htmlEmail.render(context, templates.partials),
+                ses:     {
+                    Tags: [{
+                        Name:  'type',
+                        Value: 'confirmation'
+                    }]
                 }
-                fs.readFile('src/templates/email-confirmation-text.hogan', (err, textTemplate) => {
-                    if (err) {
-                        return rej(err);
-                    }
+            }, (err) => {
+                if (err) {
+                    return rej(new Error('env:' + process.env.NODE_ENV + ' ' + JSON.stringify(err)));
+                }
 
-                    const app              = hook.app;
-                    const transporter      = app.get('nodemailerService');
-                    const fromEmail        = app.get('fromEmail');
-                    const testToEmail      = app.get('testToEmail');
-                    const confirmationLink = app.get('linksUrl') + '/confirm?id=';
-                    const htmlEmail        = hogan.compile(htmlTemplate.toString());
-                    const textEmail        = hogan.compile(textTemplate.toString());
-                    const context          = Object.assign({confirmationLink: confirmationLink}, hook.data);
-
-                    transporter.sendMail({
-                        from:    fromEmail,
-                        to:      testToEmail || context.email,
-                        subject: 'Please confirm your donation offer to aid Puerto Rico',
-                        text:    textEmail.render(context),
-                        html:    htmlEmail.render(context),
-                        ses:     {
-                            Tags: [{
-                                Name:  'type',
-                                Value: 'confirmation'
-                            }]
-                        }
-                    }, (err) => {
-                        if (err) {
-                            return rej(new Error('env:' + process.env.NODE_ENV + ' ' + JSON.stringify(err)));
-                        }
-
-                        return rsv(hook);
-                    });
-                });
+                return rsv(hook);
             });
         });
     };
