c061c16e17e906d9a6c861c8a98885a50a697b1e
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2017-10-01 13:22:53 -0400

    Implement email template styles (#4)
    * Implement email template styles; extract into partials
    
    * Soften header styles
    
    * Use .lead class instead of heading

--- src/hooks/send-email-thankyou.js
+++ src/hooks/send-email-thankyou.js
@@ -1,5 +1,17 @@
-const fs    = require('fs');
-const hogan = require('hogan.js');
+const fs        = require('fs');
+const hogan     = require('hogan.js');
+const templates = {
+    html: fs.readFileSync('src/templates/email-thankyou-html.hogan', 'utf8'),
+    text: fs.readFileSync('src/templates/email-thankyou-text.hogan', 'utf8'),
+    partials: {
+      'body-close': fs.readFileSync('src/templates/partials/email-body-close.hogan', 'utf8'),
+      'body-open': fs.readFileSync('src/templates/partials/email-body-open.hogan', 'utf8'),
+      'footer': fs.readFileSync('src/templates/partials/email-footer.hogan', 'utf8'),
+      'header': fs.readFileSync('src/templates/partials/email-header.hogan', 'utf8'),
+      'social-contact': fs.readFileSync('src/templates/partials/email-social-contact.hogan', 'utf8'),
+      'styles': fs.readFileSync('src/templates/partials/email-styles.hogan', 'utf8'),
+    },
+}
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     return function sendEmailThankyou(hook) {
@@ -8,43 +20,32 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
                 return rsv(hook);
             }
 
-            fs.readFile('src/templates/email-thankyou-html.hogan', (err, htmlTemplate) => {
+            const app         = hook.app;
+            const transporter = app.get('nodemailerService');
+            const fromEmail   = app.get('fromEmail');
+            const testToEmail = app.get('testToEmail');
+            const htmlEmail   = hogan.compile(templates.html);
+            const textEmail   = hogan.compile(templates.text);
+            const context     = hook.result.donationOffer;
+
+            transporter.sendMail({
+                from:    fromEmail,
+                to:      testToEmail || context.email,
+                subject: 'Thank you for confirming your donation offer',
+                text:    textEmail.render(context, templates.partials),
+                html:    htmlEmail.render(context, templates.partials),
+                ses:     {
+                    Tags: [{
+                        Name:  'type',
+                        Value: 'thankyou'
+                    }]
+                }
+            }, (err) => {
                 if (err) {
                     return rej(err);
                 }
-                fs.readFile('src/templates/email-thankyou-text.hogan', (err, textTemplate) => {
-                    if (err) {
-                        return rej(err);
-                    }
 
-                    const app         = hook.app;
-                    const transporter = app.get('nodemailerService');
-                    const fromEmail   = app.get('fromEmail');
-                    const testToEmail = app.get('testToEmail');
-                    const htmlEmail   = hogan.compile(htmlTemplate.toString());
-                    const textEmail   = hogan.compile(textTemplate.toString());
-                    const context     = hook.result.donationOffer;
-
-                    transporter.sendMail({
-                        from:    fromEmail,
-                        to:      testToEmail || context.email,
-                        subject: 'Thank you for confirming your donation offer',
-                        text:    textEmail.render(context),
-                        html:    htmlEmail.render(context),
-                        ses:     {
-                            Tags: [{
-                                Name:  'type',
-                                Value: 'thankyou'
-                            }]
-                        }
-                    }, (err) => {
-                        if (err) {
-                            return rej(err);
-                        }
-
-                        return rsv(hook);
-                    });
-                });
+                return rsv(hook);
             });
         });
     };
