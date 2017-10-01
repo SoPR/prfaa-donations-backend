74f8a6547683b4ab670b0223055485af276f21d3
Neil A. Beardsley <nabeards@me.com>
2017-10-01 12:50:29 -0600

    Locks down donationOffer endpoints.

--- src/services/donation-offer/donation-offer.hooks.js
+++ src/services/donation-offer/donation-offer.hooks.js
@@ -2,20 +2,19 @@ const validatDonationOffer  = require('../../hooks/validate-donation-offer');
 const setDonationFields     = require('../../hooks/set-donation-create-fields');
 const sendEmailConfirmation = require('../../hooks/send-email-confirmation');
 const updateConfirmation    = require('../../hooks/update-confirmation');
-
-const queryDonationOffer = require('../../hooks/query-donation-offer');
-
-const acceptOffer = require('../../hooks/accept-offer');
+const queryDonationOffer    = require('../../hooks/query-donation-offer');
+const acceptOffer           = require('../../hooks/accept-offer');
+const {authenticate}        = require('feathers-authentication').hooks;
 
 module.exports = {
     before: {
         all:    [],
-        find:   [queryDonationOffer()],
-        get:    [],
+        find:   [authenticate('jwt'), queryDonationOffer()],
+        get:    [authenticate('jwt')],
         create: [validatDonationOffer(), setDonationFields()],
-        update: [validatDonationOffer(), acceptOffer()],
-        patch:  [acceptOffer()],
-        remove: []
+        update: [authenticate('jwt'), validatDonationOffer(), acceptOffer()],
+        patch:  [authenticate('jwt'), acceptOffer()],
+        remove: [authenticate('jwt')]
     },
 
     after: {
